544c9dd4c3e8915acfe0774439893c7394addef8
Bagel82 <122378674+Bagel82@users.noreply.github.com>
2025-09-12 14:49:07 -0400

    Began work on SJF, mostly copying code from FCFS and altering it where needed,

--- src/SJF.java
+++ src/SJF.java
@@ -7,11 +7,50 @@ import java.util.*;
 
 public class SJF extends Algorithm {
 
+    private final List readyQueue = new LinkedList<>();
+
+    private final Queue<Process> processesToArrive;
+
+    private int now = 0;
+
     public SJF(List<Process> allProcessList){
         super(allProcessList);
+        processesToArrive = new LinkedList<>(allProcessList);
     }
 
     @Override
     public void schedule() {
+        System.out.println("Shortest Job First");
+
+        while (!readyQueue.isEmpty() || !processesToArrive.isEmpty()) {
+            if (readyQueue.isEmpty()) {
+                Process process = processesToArrive.remove();
+                if (now < process.getArrivalTime()) {
+                    //advance the simulation clock to the next process's arrival time
+                    now = process.getArrivalTime();
+                }
+                readyQueue.add(process);
+            }
+
+            Collections.sort(readyQueue);
+
+            Process currentProcess = readyQueue.remove();
+
+            int runTime = currentProcess.getBurstTime();
+
+            System.out.print("At time " + now + ": ");
+            CPU.run(currentProcess, runTime);
+
+            now += runTime;
+
+            currentProcess.setRemainingTime(0);
+            currentProcess.setFinishTime(now);
+
+            while(!processesToArrive.isEmpty() &&
+                    processesToArrive.peek().getArrivalTime()<=now) {
+                readyQueue.add(processesToArrive.remove());
+            }
+        }
+
     }
 }
