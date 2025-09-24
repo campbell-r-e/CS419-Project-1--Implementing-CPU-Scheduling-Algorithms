077b0e292c2b5610701c3deb3f7a656e3b0d8869
Bagel82 <122378674+Bagel82@users.noreply.github.com>
2025-09-24 15:03:21 -0400

    Copied over SJF code to SRTF and changed the for loop conditions.

--- src/SRTF.java
+++ src/SRTF.java
@@ -8,11 +8,59 @@ import java.util.*;
 
 public class SRTF extends Algorithm{
 
+    private final List<Process> readyProcesses = new ArrayList<>();
+
+    private final Queue<Process> processesToArrive;
+
+    private int now = 0;
+
     public SRTF(List<Process> allProcesses){
         super(allProcesses);
+        processesToArrive = new LinkedList<>(allProcesses);
     }
 
     @Override
-    public void schedule(){ }
+    public void schedule(){
+        System.out.println("Shortest Remaining Time First");
+
+        while (!readyProcesses.isEmpty() || !processesToArrive.isEmpty()) {
+            if (readyProcesses.isEmpty()) {
+                Process process = processesToArrive.remove();
+                if (now < process.getArrivalTime()) {
+                    //advance the simulation clock to the next process's arrival time
+                    now = process.getArrivalTime();
+                }
+                readyProcesses.add(process);
+            }
+
+            int smallest = -1;
+            int smallestValue = Integer.MAX_VALUE;
+
+            for(int i = 0; i < readyProcesses.size(); i ++){
+                int burst = readyProcesses.get(i).getRemainingTime();
+                if(burst < smallestValue){
+                    smallestValue = burst;
+                    smallest = i;
+                }
+            }
+
+            Process currentProcess = readyProcesses.remove(smallest);
+
+            int runTime = currentProcess.getBurstTime();
+
+            System.out.print("At time " + now + ": ");
+            CPU.run(currentProcess, runTime);
+
+            now += runTime;
+
+            currentProcess.setRemainingTime(0);
+            currentProcess.setFinishTime(now);
+
+            while(!processesToArrive.isEmpty() &&
+                    processesToArrive.peek().getArrivalTime()<=now) {
+                readyProcesses.add(processesToArrive.remove());
+            }
+        }
+    }
 
 }
